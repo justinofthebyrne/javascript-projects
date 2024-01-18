8240d71cd7ae004c57fa168d6e65a67ee72461e9
justinofthebyrne <jpburnside3@gmail.com>
2024-01-17 20:19:32 -0600

    Changes to Chapter 9 Examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,12 @@
 // create an array variable containing the names
-
+let familyNames = ["Barry", "Val", "Esther"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < familyNames.length; i++) {
+    console.log(familyNames[i]);
+} 
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -2,3 +2,8 @@
 
 
 // Write a for loop that prints each character in your name on a different line.
+let myName = "Justin Burnside";
+
+for (let i = 0; i < myName.length; i++) {
+   console.log(myName[i]);
+}
