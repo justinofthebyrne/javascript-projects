2be43b7d4a2c316939e9a6a0b17205e3dbf0b83b
justinofthebyrne <jpburnside3@gmail.com>
2024-01-20 13:59:11 -0600

    Changes to Exercises in Loops Chapter

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,9 +1,33 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
+const input = require('readline-sync');
 
+let fuelLevel = 0, numAstronauts = 0, altitude = 0
 
+while (fuelLevel <= 5000 || fuelLevel > 3000 || isNaN(fuelLevel)); {
+  fuelLevel = input.question("Enter the starting fuel level: ");
+      
+}
 
+while (numAstronauts >= 1 || numAstronauts <= 7 || isNaN(numAstronauts)); {
+  numAstronauts = input.question("Enter the number of astronauts aboard: ");
+      
+}
 
+while (numAstronauts >= 5 || numAstronauts <= 10 || isNaN(numAstronauts)); {
+  numAstronauts = input.question("Enter the number of astronauts aboard: ");
+      
+}
+
+while (fuelLevel-100*numAstronauts >= 0) {
+  altitude += 50; 
+  fuelLevel -= 100*numAstronauts;
+}
+
+let output = `The shuttle gained an altitude of ${altitude} km.`; 
+if (altitude >= 2000) {
+  output += "Orbit Achieved!"; 
+}
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
